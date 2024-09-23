5a62178a2dff60f22478f72e1845689141b719b7
farazal9 <farazxwave@gmail.com>
2024-09-23 13:38:22 +0500

    React functions

---
+++ src/HeroSection.js
@@ -0,0 +1,10 @@
+const HeroSection = () =>{
+    return(
+        <>
+        <p>
+            dfbdfshfgsdhyfgsdcfhdgchdbchdcydhcfdycdbvgycdgcfdhycgdycfsdfhysdgfyfcgdrfvfhjyfyufyudeyfgdf
+        </p>
+        </>
+    )
+}
+export default HeroSection
